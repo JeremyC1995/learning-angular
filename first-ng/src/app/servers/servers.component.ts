7e90e4973577979a7344dffa05b523254e2763a0
Jeremy <cssj1995@gmail.com>
2018-05-13 00:57:18 +0800

    up to two-way-databinding

---
+++ first-ng/src/app/servers/servers.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'no server added';
+  serverName='';
+
+  constructor() { 
+    setTimeout(()=>{
+      this.allowNewServer = true;
+    },2000);
+  }
+
+  ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus= 'server was created';
+  }
+  onUpdateServerName(e: Event) {
+    this.serverName = (<HTMLInputElement>e.target).value;
+  }
+}
