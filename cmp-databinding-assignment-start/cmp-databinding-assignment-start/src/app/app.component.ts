e3879296d97be362e44c876602f308a208321c9a
Jeremy Cheng <jeremych@hk1.ibm.com>
2018-05-14 18:41:35 +0800

    adding btn

--- cmp-databinding-assignment-start/cmp-databinding-assignment-start/src/app/app.component.ts
+++ cmp-databinding-assignment-start/cmp-databinding-assignment-start/src/app/app.component.ts
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  odd: number[];
+  even: number[];
+
+  onStart(e){
+    console.log(e);
+  }
 }
